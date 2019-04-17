1193fd6d148c0260c3973e648f52e804315971c8
github username <github email address>
2019-04-17 07:39:37 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,13 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  reformed_hash = {}
+  languages.each do |style, language|
+    language.each do |name, type|
+      reformed_hash[name] ||= type
+      reformed_hash[name][:style] ||= []
+      reformed_hash[name][:style] << style
+    end
+  end
+  reformed_hash
 end
